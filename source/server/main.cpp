8e6c587c2fe619e83d4a4825eb755a28da1070de
Fabien <fabien.brosset@epitech.eu>
2019-11-27 14:25:56 +0100

    new branch

--- source/server/main.cpp
+++ source/server/main.cpp
@@ -5,19 +5,19 @@
 ** server/main.cpp
 */
 
-#include "server.hpp"
+#include "Server.hpp"
 
-int main()
+int main(int ac, char *av[])
 {
     try
     {
         boost::asio::io_context ioContext;
 //        std::unique_ptr<AServer> s(new server(io_context, std::stoi(argv[1])));
-        server server(ioContext);
+        Server server(ioContext, std::stoi(av[1]));
         ioContext.run();
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
         return (84);
     }
-    return 0;
+    return (0);
 }
