58a20d343c268fbc7c0e969f2c658d8450fc5cd1
Fabien <fabien.brosset@epitech.eu>
2019-12-01 20:00:01 +0100

    documenting clientNetwork for doxygen

--- source/client/ClientNetwork.hpp
+++ source/client/ClientNetwork.hpp
@@ -14,13 +14,49 @@
 class ClientNetwork
 {
 public:
+
+    /**
+     * Initialise the connection by calling the abstract network class
+     */
     ClientNetwork();
+
+    /**
+     * Connection to the server
+     * @param host is the server address
+     * @param port is the opened server port
+     * @return true
+     * @return false
+     */
     bool connect(const std::string &host, const std::string &port);
+
+    /**
+     * Client disconnection
+     * @return true
+     * @return false
+     */
     bool disconnect();
 
+    /**
+     * send Events to the server
+     * @param events is a binary packet filled by events
+     * @return true
+     * return false
+     */
     bool sendEvents(const Events &events);
+
+    /**
+     * Get entities list
+     * @return a list of entities to display
+     */
     std::vector<SerializedEntity> getEntities();
 private:
+
+    /**
+     * Convert an entity packet to a list of entities
+     * @param packet is the entity packet to serialized
+     * @return true
+     * @return false
+     */
     SerializedEntity entityPacketToSerialized(const Protocol::PacketManager::Entity &packet);
     bool findIdExist(int, std::vector<int>);
 
